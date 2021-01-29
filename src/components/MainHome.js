430fe14a1c53f6efe8baec55b11332c5979f013e
orangeappleak <appannagarikarthik.123@gmail.com>
2021-01-29 18:54:22 +0530

    just a regular commit

--- src/components/MainHome.js
+++ src/components/MainHome.js
@@ -9,19 +9,6 @@ export default function MainHome(){
             <div id="employees-illustration">
                 <img src="https://assets.website-files.com/5bff8886c3964a992e90d465/5c00621b7aefa4f9ee0f4303_wide-shot.svg"></img>
             </div>
-            <div id="main-page-heading">
-                <h1>Its easier to manage your teams and employees now.</h1>
-            </div>
-            <div id="main-page-details">
-                <h1>This is where the word changes.</h1>
-            </div>
-            <div id="new-div">
-                <h1>This is a new div.</h1>
-            </div>
-            <div id="new-div-added">
-                <h1>added from other</h1>
-            </div>
-
         </div>
     )
 }
